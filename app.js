7c07565b3d6d3040ffdc31545d75f4e15bf7c337
AidaDaiyrbekova <hgirl0108@gmail.com>
2025-07-17 21:20:12 +0600

    test1

---
+++ app.js
@@ -0,0 +1,52 @@
+// Задача 1
+
+// const regExp = /^\d+$/
+//
+// const containsOnlyDigits = (str) => {
+//       return regExp.test(str)
+// }
+// console.log(containsOnlyDigits("45488"))
+// console.log(containsOnlyDigits("787fd85"))
+
+
+// Задача 2
+// const interval= setInterval(() => {
+//  console.log('Прошла секунда!')
+// }, 1000)
+
+
+// Задача 3
+// const count = () => {
+//     let i = 1
+//     const interval = setInterval(() => {
+//         console.log(i)
+//         if (i === 10) {
+//             clearInterval(interval)
+//         }
+//         i++
+//     }, 1000)
+// }
+// count()
+
+// Задача 4
+// const button = document.querySelector('#btn')
+// const box = document.querySelector('#box')
+//
+// button.addEventListener('click', () => {
+//     box.classList.toggle('colored')
+// })
+
+
+
+// Задача 5
+//     const xhr = new XMLHttpRequest()
+//     console.log(xhr)
+//     xhr.open('GET', 'test.json') //
+//     xhr.setRequestHeader('Content-type', 'application/json')
+//     xhr.send()
+//
+// xhr.onload = () => {
+//     const data = JSON.parse(xhr.response)
+//     console.log(data)
+// }
+
